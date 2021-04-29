9b67f3efaee7ef7f6ac7a42db123c9b5edcb72a5
LNdame <ls20045@gmail.com>
2021-04-29 22:35:25 +0200

    init commit

---
+++ android/app/src/main/kotlin/co/za/inqb8/gogo_online/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.za.inqb8.gogo_online
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/models/user.dart
@@ -0,0 +1,82 @@
+import 'role.dart';
+import '../models/media.dart';
+
+class User {
+  String id;
+  String name;
+  String email;
+  String password;
+  String apiToken;
+  String deviceToken;
+  String phone;
+  String address;
+  String bio;
+  Media image;
+  Role role;
+  // used for indicate if client logged in or not
+  bool auth;
+
+//  String role;
+
+  User();
+
+  User.fromJSON(Map<String, dynamic> jsonMap) {
+    try {
+      id = jsonMap['id'].toString();
+      name = jsonMap['name'] != null ? jsonMap['name'] : '';
+      email = jsonMap['email'] != null ? jsonMap['email'] : '';
+      apiToken = jsonMap['api_token'];
+      deviceToken = jsonMap['device_token'];
+      try {
+        phone = jsonMap['custom_fields']['phone']['view'];
+      } catch (e) {
+        phone = "";
+      }
+      try {
+        address = jsonMap['custom_fields']['address']['view'];
+      } catch (e) {
+        address = "";
+      }
+      try {
+        bio = jsonMap['custom_fields']['bio']['view'];
+      } catch (e) {
+        bio = "";
+      }
+      image = jsonMap['media'] != null && (jsonMap['media'] as List).length > 0 ? Media.fromJSON(jsonMap['media'][0]) : new Media();
+      role = jsonMap['roles'] != null && (jsonMap['roles'] as List).length > 0
+          ? Role.fromJSON(jsonMap['roles'][0])
+          : new Role();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Map toMap() {
+    var map = new Map<String, dynamic>();
+    map["id"] = id;
+    map["email"] = email;
+    map["name"] = name;
+    map["password"] = password;
+    map["api_token"] = apiToken;
+    if (deviceToken != null) {
+      map["device_token"] = deviceToken;
+    }
+    map["phone"] = phone;
+    map["address"] = address;
+    map["bio"] = bio;
+    map["media"] = image?.toMap();
+    map['roles'] = role?.toMap();
+    return map;
+  }
+
+  @override
+  String toString() {
+    var map = this.toMap();
+    map["auth"] = this.auth;
+    return map.toString();
+  }
+
+  bool profileCompleted() {
+    return address != null && address != '' && phone != null && phone != '';
+  }
+}
