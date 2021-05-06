8617d4e93519dde80a38a3f15b5050e309751907
LNdame <ls20045@gmail.com>
2021-05-06 21:32:19 +0200

    [feature] register healer first step

--- lib/src/controllers/healer_controller.dart
+++ lib/src/controllers/healer_controller.dart
@@ -121,53 +121,31 @@ class HealerController extends ControllerMVC {
     listenForFeaturedProducts(_id);
   }
 
-  void requestRegisterHealer(Healer healer) async {
+  void requestRegisterHealer(Healer healer) async{
     loader = Helper.overlayLoader(state.context);
     FocusScope.of(state.context).unfocus();
-    if (registerFormKey.currentState.validate()) {
+    //if(registerFormKey.currentState.validate()){
       registerFormKey.currentState.save();
       Overlay.of(state.context).insert(loader);
-      registerHealer(healer).then((value) {
-        if (value != null) {
-          Product newConsultation = setupVirtualConsultation(healer.hourlyPrice, value);
-          storeProduct(newConsultation).then((value) {
-            if (value != null) {
-              ScaffoldMessenger.of(scaffoldKey?.currentContext).showSnackBar(SnackBar(
-                content: Text("Request sent and consultation rate set!"),
-              ));
-            }
-          });
-
-          Navigator.of(state.context).pushReplacementNamed('/HealerRegisterSuccess');
-        } else {
+      registerHealer(healer).then( (value){
+        if(value!=null){
+          //TODO navigate to request success
+          ScaffoldMessenger.of(scaffoldKey?.currentContext).showSnackBar(SnackBar(
+            content: Text("Registered!!"),
+          ));
+        }else{
           ScaffoldMessenger.of(scaffoldKey?.currentContext).showSnackBar(SnackBar(
             content: Text("Error!!"),
           ));
         }
-      }).catchError((error) {
+      }).catchError((error){
         loader.remove();
         ScaffoldMessenger.of(scaffoldKey?.currentContext).showSnackBar(SnackBar(
           content: Text("Error!!"),
         ));
       }).whenComplete(() => Helper.hideLoader(loader));
+
+
     }
-  }
-
-  Product setupVirtualConsultation(double price, Healer healer){
-    var product = new Product();
-    product.name = "Virtual Consultation";
-    product.price = price;
-    product.description ="Chat or video call with the healer";
-    product.capacity = "1";
-    product.packageItemsCount="1";
-    product.unit = "per hour";
-    product.featured = false;
-    product.deliverable = false;
-    product.healer = healer;
-
-    return product;
-  }
-
-
-  /// TODO retrieve chat data with current user if exist
+  //}
 }
