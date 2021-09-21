a4918b49876173e1d8add1dfc91ab327fe6abd31
LoicASN <lndame@jhb.dvt.co.za>
2021-09-21 18:12:43 +0200

    remove unwanted call

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:global_configuration/global_configuration.dart';
@@ -28,7 +29,7 @@ Future<void> main() async {
   print(CustomTrace(StackTrace.current, message: "base_url: ${GlobalConfiguration().getString('base_url')}"));
   print(CustomTrace(StackTrace.current, message: "api_base_url: ${GlobalConfiguration().getString('api_base_url')}"));
   HttpOverrides.global = new MyHttpOverrides();
-
+ // FirebaseApp app = await FirebaseApp.configure(name: "Gogo_online", options: null);
   // Set `enableInDevMode` to true to see reports while in debug mode
   // This is only to be used for confirming that reports are being
   // submitted as expected. It is not intended to be used for everyday
